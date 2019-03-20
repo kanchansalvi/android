41c05dd2114851dcd44296d3133092baac25c0e3
kanchansalvi <deepkanchan.sonawane@gmail.com>
2019-03-19 21:15:48 -0700

    Add Android TextViewDemo7 Project
    This project shows how to create and use MultiAutoCompleteTextView with custom Tokens in Android

---
+++ uiControls/TextViewDemo7/app/src/main/java/com/android/uiControls/MainActivity.java
@@ -0,0 +1,37 @@
+package com.android.uiControls;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTxt;
+    private MultiAutoCompleteTextView mColorAutoTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTxt = findViewById(R.id.txtID);
+        mColorAutoTxt = findViewById(R.id.colorAutoTxt);
+
+        final String[] colors = {"Red", "Blue", "Green","Orange", "Grey", "White", "Black"};
+
+        // Starts searching the characters in color from first character
+        mColorAutoTxt.setThreshold(1);
+
+        // Creating ArrayAdapter for the List of Colors
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.select_dialog_item, colors);
+        // Set the Adapter data into Autocomplete textView
+        mColorAutoTxt.setAdapter(adapter);
+
+        // Set so that words when seperated by comma is displayed in list
+        mColorAutoTxt.setTokenizer(new SpaceTokenizer());
+
+    }
+}
