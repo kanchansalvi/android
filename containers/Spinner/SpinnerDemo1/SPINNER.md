47277fa66bac90af3c1e0c0a240392f38118de0d
kanchansalvi <deepkanchan.sonawane@gmail.com>
2019-03-26 23:13:45 -0700

    Add Android Spinner Projects
    This project shows how to create and use Spinner in Android

---
+++ containers/Spinner/SpinnerDemo1/SPINNER.md
@@ -0,0 +1,36 @@
+# SPINNER
+Repo for all Spinner Container in Android Projects
+
+# Spinner :
+
+- Used to select one value from a set of values. when touched to spinner, 
+it displays the list of values from where the desired value is to be selected.  
+- By default, currently/first selected value is displayed.
+- Adapter class needs to be used as Spinner is associated with AdapterView.
+
+other description with program examples....
+
+
+Links:
+
+[Link 1](https://developer.android.com/guide/topics/ui/controls/spinner) ,
+[Link 2](https://developer.android.com/reference/android/widget/Spinner) , 
+[Link 3](https://www.mkyong.com/android/android-spinner-drop-down-list-example/) , 
+[Link 4](https://www.javatpoint.com/android-spinner-example) , 
+[Link 5](https://abhiandroid.com/ui/spinner) , 
+[Link 6](https://www.journaldev.com/9231/android-spinner-drop-down-list) , 
+[Link 7](https://www.tutlane.com/tutorial/android/android-spinner-dropdown-list-with-examples)
+
+
+# Custom Spinner :
+
+A custom spinner is needed when we want to display more than one item in 
+spinner. This can be created by creating a Custom Adapter class and 
+extending it to the BaseAdapter class.
+Follow this example [SpinnerDemo2]() to create a custom Spinner with text and Image.
+
+[Link 1](https://abhiandroid.com/ui/custom-spinner-examples.html) ,
+[Link 2](https://www.edureka.co/blog/custom-spinner-in-android) , 
+[Link 3](https://www.codingdemos.com/android-custom-spinner-images-text/) , 
+
+
---
+++ containers/Spinner/SpinnerDemo1/app/src/main/java/com/android/containers/MainActivity.java
@@ -0,0 +1,59 @@
+package com.android.containers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private TextView mCountryTxt;
+    private Spinner mCountrySpinner ;
+    private List mCountryList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCountryTxt = findViewById(R.id.countryTxtID);
+        mCountrySpinner = findViewById(R.id.countrySpinnerID);
+
+        mCountrySpinner.setOnItemSelectedListener(this);
+
+        // Initialize the mCountryList with some data
+        initializeCountryList();
+
+        // Specify an Adapter
+        final ArrayAdapter<String> countryAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,mCountryList);
+        mCountrySpinner.setAdapter(countryAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) { }
+
+    private void initializeCountryList(){
+        mCountryList = new ArrayList();
+        mCountryList.add("Captain Marvel");
+        mCountryList.add("The NutCracker");
+        mCountryList.add("Beauty and the Beast");
+        mCountryList.add("Frozen");
+        mCountryList.add("Hotel Transylvania");
+    }
+}
