9fa8b09cf8425480320900933c6ce3fe12e1f7d7
kanchansalvi <deepkanchan.sonawane@gmail.com>
2019-03-28 22:04:07 -0700

    Add Android ConstraintGuideline Projects
    This project shows how to create and use Horizontal Constraint Guideline in Android

---
+++ layouts/ConstraintGuideline/ConstraintGuidelineHorizontalDemo1/app/src/main/java/com/android/layouts/MainActivity.java
@@ -0,0 +1,24 @@
+package com.android.layouts;
+
+import android.graphics.Rect;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.TransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public MainActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
