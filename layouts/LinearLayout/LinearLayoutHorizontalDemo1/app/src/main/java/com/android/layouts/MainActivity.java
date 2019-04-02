a78869d4d997841190d00aa7b9061c3e67185649
kanchansalvi <deepkanchan.sonawane@gmail.com>
2019-04-01 22:03:31 -0700

    Add changes to activity file

--- layouts/LinearLayout/LinearLayoutHorizontalDemo1/app/src/main/java/com/android/layouts/MainActivity.java
+++ layouts/LinearLayout/LinearLayoutHorizontalDemo1/app/src/main/java/com/android/layouts/MainActivity.java
@@ -6,17 +6,10 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mStartBtn;
-    private Button mPauseBtn;
-    private Button mStopBtn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mStartBtn = findViewById(R.id.startBtnID);
-        mPauseBtn = findViewById(R.id.pauseBtnID);
-        mStopBtn = findViewById(R.id.stopBtnID);
     }
 }
