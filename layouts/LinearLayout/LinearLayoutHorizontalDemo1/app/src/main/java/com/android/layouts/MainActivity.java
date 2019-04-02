072e425feb991db2e98f8a6488e3d42f6c128d97
kanchansalvi <deepkanchan.sonawane@gmail.com>
2019-04-02 16:04:49 -0700

    Add update to file

--- layouts/LinearLayout/LinearLayoutHorizontalDemo1/app/src/main/java/com/android/layouts/MainActivity.java
+++ layouts/LinearLayout/LinearLayoutHorizontalDemo1/app/src/main/java/com/android/layouts/MainActivity.java
@@ -3,13 +3,22 @@ package com.android.layouts;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mStopTxt;
+    private TextView mPauseTxt;
+    private TextView mStartTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStopTxt = findViewById(R.id.stopTxtID);
+        mPauseTxt = findViewById(R.id.pauseTxtID);
+        mStartTxt = findViewById(R.id.startTxtID);
 
     }
 }
