4de2812328496989fdbed585946f541ea72e62bd
kanchansalvi <deepkanchan.sonawane@gmail.com>
2019-04-08 16:07:04 -0700

    Add Android NavigationView Projects
    This project shows how to create and use NavigationView in Android

---
+++ containers/NavigationView/NAVIGATIONVIEW.md
@@ -0,0 +1,20 @@
+# nAVIGATIONVIEW :
+Repo for Container NavigationView in Android Projects
+
+# NavigationView :
+
+1) 
+The program [NavigationViewDemo1](NavigationViewDemo1) creates a Navigation Drawer.    
+
+
+Steps:
+1) Add gradle dependencies:
+implementation 'com.android.support:appcompat-v7:28.0.0'
+ implementation 'com.android.support:design:28.0.0'
+    
+Links:
+
+[Link 1](https://developer.android.com/guide/topics/ui/layout/cardview),
+[Link 2](https://developer.android.com/reference/android/support/v7/widget/CardView)
+
+
---
+++ containers/NavigationView/NavigationViewDemo1/app/src/main/java/com/android/containers/MainActivity.java
@@ -0,0 +1,56 @@
+package com.android.containers;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbarID);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerID);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,0,0);
+        //drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationID);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerID);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
